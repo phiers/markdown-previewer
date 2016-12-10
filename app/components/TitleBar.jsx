4be328df6627b9e09bfc92223efb09cf5273d4dc
Paul Hiers Jr <paulhiers@yahoo.com>
2016-12-10 13:54:16 -0500

    small changes at the last minute

--- app/components/TitleBar.jsx
+++ app/components/TitleBar.jsx
@@ -3,11 +3,8 @@ import React from 'react';
 const TitleBar = () => (
   <div className="top-bar">
     <div className="top-bar-left">
-      <p className="menu-text">Markdown Previewer for GFM</p>
-    </div>
-    <div className="top-bar-left">
-      <p className="menu-text">
-        <a href="https://help.github.com/articles/basic-writing-and-formatting-syntax/" rel="noopener noreferrer" target="_blank">More info</a>
+      <p className="menu-text">Markdown Previewer for GFM
+        <a href="https://help.github.com/articles/basic-writing-and-formatting-syntax/" rel="noopener noreferrer" target="_blank">  (more info)</a>
       </p>
     </div>
     <div className="top-bar-right">
